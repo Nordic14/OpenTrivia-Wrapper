03441d8cf99eb9c9053da380c54355452e66fc3e
Diogo <diogo6204@gmail.com>
2020-12-07 21:04:22 +0000

    Add project files.

---
+++ OpenTrivia Wrapper/OpenTriviaClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OpenTrivia_Wrapper
+{
+    public class OpenTriviaClient
+    {
+        internal const string BASE_URL = ""; 
+
+    }
+}
---
+++ TestWrapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
